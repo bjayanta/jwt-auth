d77085685202ca6411d387d8f5e2392951623037
bjayanta <bjayanta.me@gmail.com>
2022-07-09 11:35:27 +0600

    add postgresql

--- jwt_auth/settings.py
+++ jwt_auth/settings.py
@@ -78,17 +78,25 @@ WSGI_APPLICATION = 'jwt_auth.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-    'default': {
+    'sqlite3': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    'mysql': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'jwt_auth',
         'USER': 'root', 
         'PASSWORD': 'rootUser@2022',
         'HOST': 'localhost',
         'PORT': '3306',
+    },
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'jwt_auth',
+        'USER': 'postgres', 
+        'PASSWORD': 'root@2022',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
